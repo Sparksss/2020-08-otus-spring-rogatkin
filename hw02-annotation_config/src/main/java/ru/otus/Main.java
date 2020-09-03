f2ac0bb9dd3237168bbd7f211719ee41390bce56
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-09-03 12:45:29 +0300

    initialize application

---
+++ hw02-annotation_config/src/main/java/ru/otus/Main.java
@@ -0,0 +1,19 @@
+package ru.otus;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import ru.otus.services.StudentService;
+import ru.otus.services.StudentServiceImpl;
+
+/**
+ * Created by ilya on Sep, 2020
+ */
+@ComponentScan
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
+        StudentService studentService = context.getBean(StudentServiceImpl.class);
+        studentService.greeting();
+        studentService.testing();
+    }
+}
