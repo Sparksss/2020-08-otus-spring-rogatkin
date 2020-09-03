08290140d2a0a6039abd0a466f8bbaf632c3c431
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-09-03 12:45:14 +0300

    add domain

---
+++ hw02-annotation_config/src/main/java/ru/otus/domain/Student.java
@@ -0,0 +1,31 @@
+package ru.otus.domain;
+
+/**
+ * Created by ilya on Sep, 2020
+ */
+public class Student {
+
+    private String firstName;
+    private String lastName;
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
