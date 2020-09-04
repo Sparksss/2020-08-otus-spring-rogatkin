793ba25201a5be5b550f38d082eb95473898389f
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-09-04 22:21:26 +0300

    initialize IO in IOConfig bean

---
+++ hw02-annotation_config/src/main/java/ru/otus/configuration/IOConfig.java
@@ -0,0 +1,35 @@
+package ru.otus.configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by ilya on Sep, 2020
+ */
+@PropertySource("classpath:application.properties")
+@Configuration
+public class IOConfig {
+
+    @Value("${file.name}")
+    private String fileName;
+    private static Logger logger = LoggerFactory.getLogger(IOConfig.class);
+
+    @Bean
+    public String content() {
+        final ClassLoader classLoader = getClass().getClassLoader();
+        String content = null;
+        try (InputStream stream = classLoader.getResourceAsStream(fileName)) {
+            content = new String(stream.readAllBytes());
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+        return content;
+    }
+}
