fd6dcb46188977a558be93f0d18f4e334c71e9c6
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-09-04 22:19:16 +0300

    move IO to config

--- hw02-annotation_config/src/main/java/ru/otus/dao/StudentDaoImpl.java
+++ hw02-annotation_config/src/main/java/ru/otus/dao/StudentDaoImpl.java
@@ -3,51 +3,42 @@ package ru.otus.dao;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.otus.domain.Student;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
  * Created by ilya on Aug, 2020
  */
+
 @Repository
 public class StudentDaoImpl implements StudentDao {
 
-    private String fileName = "questions.csv";
     private static Logger logger = LoggerFactory.getLogger(StudentDaoImpl.class);
 
-    private final ClassLoader classLoader = getClass().getClassLoader();
+    private final String content;
 
-    private String readFile() {
-        String content = null;
-        try (InputStream stream = classLoader.getResourceAsStream(fileName)) {
-            content = new String(stream.readAllBytes());
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-        return content;
+    @Autowired
+    public StudentDaoImpl(String content) {
+        this.content = content;
     }
 
     @Override
     public List<String> getQuestions() {
-        String content = this.readFile();
         List<String> questions = new ArrayList<>();
-        content = content.replaceAll("(,.+\n)", "\n");
         String[] str = content.split("\n");
+
         for(int i = 1; i < str.length; i++) {
-            questions.add(str[i]);
+            questions.add(str[i].replaceAll("(,.+)", ""));
         }
         return questions;
     }
 
     @Override
     public List<String> getRightAnswers() {
-        String content = this.readFile();
         String[] str = content.split(",|\n");
         List<String> answers = new ArrayList<>();
         for(int i = 4; i < str.length; i += 3) {
