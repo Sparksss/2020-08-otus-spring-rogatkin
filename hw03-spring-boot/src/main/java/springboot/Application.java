b09645706ac265f4a714ef4eb2019ed8b9c8edbf
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-10-06 23:11:59 +0300

    moved files from main module to hw03

--- hw03-spring-boot/src/main/java/springboot/Application.java
+++ hw03-spring-boot/src/main/java/springboot/Application.java
@@ -2,13 +2,17 @@ package springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springboot.services.StudentService;
+import springboot.services.StudentServiceImpl;
 
 /**
  * Created by ilya on Oct, 2020
  */
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    public static void main(String[] args) throws Exception {
+        var context = SpringApplication.run(Application.class, args);
+        StudentService studentService = context.getBean(StudentServiceImpl.class);
+        studentService.testing();
     }
 }
