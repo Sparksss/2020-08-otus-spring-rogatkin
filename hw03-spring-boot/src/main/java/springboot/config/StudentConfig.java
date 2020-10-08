f3ca6c852d5c10c5bce9952e1433b139f4b61a7d
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-10-08 23:10:08 +0300

    remove auto configuration for configs

--- hw03-spring-boot/src/main/java/springboot/config/StudentConfig.java
+++ hw03-spring-boot/src/main/java/springboot/config/StudentConfig.java
@@ -1,12 +1,10 @@
 package springboot.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by ilya on Oct, 2020
  */
-@Configuration
 @ConfigurationProperties(prefix = "student")
 public class StudentConfig {
     private int grade;
