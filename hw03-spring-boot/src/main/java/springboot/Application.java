1208ce5dd77bcbf6a910367b2fffa46a895a22a6
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-10-08 23:08:22 +0300

    add service testing run

--- hw03-spring-boot/src/main/java/springboot/Application.java
+++ hw03-spring-boot/src/main/java/springboot/Application.java
@@ -2,6 +2,10 @@ package springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import springboot.config.LocaleConfig;
+import springboot.config.QuestionsConfig;
+import springboot.config.StudentConfig;
 import springboot.services.StudentService;
 import springboot.services.StudentServiceImpl;
 
@@ -9,6 +13,7 @@ import springboot.services.StudentServiceImpl;
  * Created by ilya on Oct, 2020
  */
 @SpringBootApplication
+@EnableConfigurationProperties({QuestionsConfig.class, StudentConfig.class, LocaleConfig.class})
 public class Application {
     public static void main(String[] args) throws Exception {
         var context = SpringApplication.run(Application.class, args);
