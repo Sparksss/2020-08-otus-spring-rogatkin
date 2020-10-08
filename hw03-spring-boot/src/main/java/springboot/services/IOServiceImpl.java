c58f580811842341b683816c81c180404651002a
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-10-08 23:08:59 +0300

    add change file for questions by locale

--- hw03-spring-boot/src/main/java/springboot/services/IOServiceImpl.java
+++ hw03-spring-boot/src/main/java/springboot/services/IOServiceImpl.java
@@ -3,6 +3,7 @@ package springboot.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import springboot.config.LocaleConfig;
 import springboot.config.QuestionsConfig;
 
 import java.io.IOException;
@@ -12,18 +13,20 @@ import java.io.InputStream;
 public class IOServiceImpl implements IOService {
 
     private final String fileName;
+    private final LocaleConfig localeConfig;
 
     private static Logger logger = LoggerFactory.getLogger(IOServiceImpl.class);
 
-    public IOServiceImpl(QuestionsConfig questionsConfig) {
+    public IOServiceImpl(QuestionsConfig questionsConfig, LocaleConfig localeConfig) {
         this.fileName = questionsConfig.getFileName();
+        this.localeConfig = localeConfig;
     }
 
     @Override
     public String readFile() {
         final ClassLoader classLoader = getClass().getClassLoader();
         String content = null;
-        try (InputStream stream = classLoader.getResourceAsStream(fileName)) {
+        try (InputStream stream = classLoader.getResourceAsStream("i18n/"+ localeConfig.getLocale().toString() + '/' + fileName)) {
             content = new String(stream.readAllBytes());
         } catch (IOException e) {
             logger.error(e.getMessage());
