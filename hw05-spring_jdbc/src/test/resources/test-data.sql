9e3737a6328a20288636e1d24444d5b3ec6bed32
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-10-14 16:33:33 +0300

    add test data

---
+++ hw05-spring_jdbc/src/test/resources/test-data.sql
@@ -0,0 +1,10 @@
+insert into genres (name)
+values ('Romance');
+insert into genres (name)
+values ('Comedy');
+insert into authors(name)
+values ('Anton Chekhov');
+insert into authors (name)
+values ('Alexander Pushkin');
+insert into books (name, genre_id)
+values ('My Live Story', 2);
