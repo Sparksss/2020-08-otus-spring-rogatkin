1ee3648d0464fa94db99cf02e8526b926f09b163
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-10-14 16:28:58 +0300

    add books_authors table

--- hw05-spring_jdbc/src/main/resources/scheme.sql
+++ hw05-spring_jdbc/src/main/resources/scheme.sql
@@ -11,6 +11,11 @@ CREATE TABLE authors (
 CREATE TABLE books (
     id serial primary key,
     name varchar(200) not null,
-    genre_id int references(genres.id)
-    author_id int references (authors.id)
+    genre_id int references genres(id)
+);
+
+CREATE TABLE books_authors (
+    id serial primary key,
+    book_id int references books(id),
+    author_id int references authors(id)
 );
