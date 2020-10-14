d38ed877223582bd797f1881df38a0a727619d6c
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-10-14 16:34:09 +0300

    test BookService and add new row

--- hw05-spring_jdbc/src/main/java/ru/otus/Application.java
+++ hw05-spring_jdbc/src/main/java/ru/otus/Application.java
@@ -2,6 +2,10 @@ package ru.otus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import ru.otus.domains.Book;
+import ru.otus.services.BookService;
+
 
 /**
  * Created by ilya on Oct, 2020
@@ -9,6 +13,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+       ConfigurableApplicationContext ctx =  SpringApplication.run(Application.class);
+       BookService bookService = ctx.getBean(BookService.class);
+       Book book = bookService.findById(1);
+        System.out.println(book.getName());
+       //       Book book = new Book("Algebra");
+//       bookService.save(book);
+//       for(Book book1 : books) {
+//           System.out.println(book1.getName());
+//       }
     }
 }
