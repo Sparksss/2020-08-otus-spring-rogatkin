9f42c72b67159e3da3d4dd53aca57999ef306d9f
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-11-04 00:25:30 +0300

    refactoring GenreService

--- hw05-spring_jdbc/src/main/java/ru/otus/services/GenreService.java
+++ hw05-spring_jdbc/src/main/java/ru/otus/services/GenreService.java
@@ -7,6 +7,7 @@ import ru.otus.domains.Genre;
 
 import java.util.List;
 
+
 @Component
 public class GenreService {
 
@@ -17,18 +18,19 @@ public class GenreService {
         this.genreDaoJdbc = genreDaoJdbc;
     }
 
-    public List<Genre> getAll() {
+    public List<Genre> findAll() {
         return this.genreDaoJdbc.getAll();
     }
 
-    public Genre getById(long id) throws Exception {
-        if(id == 0) throw new Exception("Wrong id");
-        return this.genreDaoJdbc.getById(id);
+    public Genre findById(long id) throws Exception {
+        return this.getById(id);
     }
 
     public Genre getByName(String name) throws Exception {
         if(name == null || name.isEmpty()) throw new Exception("Please enter a name of genre");
-        return this.genreDaoJdbc.getByName(name);
+        Genre genre = this.genreDaoJdbc.getByName(name);
+        if(genre == null) throw new Exception(String.format("%s%s%s", "Genre with name",name,"does not exists"));
+        return genre;
     }
 
     public int countAll() {
@@ -40,10 +42,21 @@ public class GenreService {
         this.genreDaoJdbc.insert(genre);
     }
 
-    public void update(Genre genre) throws Exception {
-        if(genre == null) throw new Exception("Genre is null");
+    public void update(long id, String genreName) throws Exception {
+        Genre genre = this.getById(id);
+        genre.setName(genreName);
         this.genreDaoJdbc.update(genre);
     }
 
+    public void delete(long id) throws Exception {
+        this.genreDaoJdbc.delete(this.getById(id));
+    }
+
+    private Genre getById(long id) throws Exception {
+        if(id == 0) throw new Exception("Wrong parameter id");
+        Genre genre = this.genreDaoJdbc.getById(id);
+        if(genre == null) throw new Exception(String.format("%s%s%s", "Genre with id",id,"does not exists"));
+        return genre;
+    }
 
 }
