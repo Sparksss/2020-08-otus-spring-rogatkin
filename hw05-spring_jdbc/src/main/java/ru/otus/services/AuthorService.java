577903a3cd768b464768d565791f28310950f850
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-11-04 00:25:16 +0300

    refactoring AuthorService

--- hw05-spring_jdbc/src/main/java/ru/otus/services/AuthorService.java
+++ hw05-spring_jdbc/src/main/java/ru/otus/services/AuthorService.java
@@ -6,6 +6,7 @@ import ru.otus.domains.Author;
 
 import java.util.List;
 
+
 @Component
 public class AuthorService {
     private AuthorDaoJdbc authorDaoJdbc;
@@ -20,32 +21,28 @@ public class AuthorService {
     }
 
     public void update(long id, String authorName) throws Exception {
-        if(id == 0) throw new Exception("Wrong parameter id");
-        if(authorName == null || authorName.isEmpty()) throw new Exception("Please fill author name");
-        Author author = this.authorDaoJdbc.getById(id);
-        if(author == null) throw new Exception("Author with id" + id + "does not exists");
+        Author author = this.getById(id);
         author.setName(authorName);
         this.authorDaoJdbc.update(author);
     }
 
     public void delete(long id) throws Exception {
+        this.authorDaoJdbc.delete(this.getById(id));
+    }
+
+    public List<Author> findAll() {
+        return this.authorDaoJdbc.getAll();
+    }
+
+    public Author findById(long id) throws Exception {
+        if(id == 0) throw new Exception("Wrong parameter id");
+        return this.getById(id);
+    }
+
+    private Author getById(long id) throws Exception {
         if(id == 0) throw new Exception("Wrong parameter id");
         Author author = this.authorDaoJdbc.getById(id);
         if(author == null) throw new Exception("Author with id" + id + "does not exists");
-        this.authorDaoJdbc.delete(author);
-    }
-
-    public void findAll() {
-        List<Author> authors = this.authorDaoJdbc.getAll();
-        for(Author author : authors) {
-            System.out.println(author);
-        }
-    }
-
-    public void findById(long id) throws Exception {
-        if(id == 0) throw new Exception("Wrong parameter id");
-        Author author = this.authorDaoJdbc.getById(id);
-        if(author == null) throw new Exception("Author with id" + id + "does not exists");
-        System.out.println(author);
+        return author;
     }
 }
