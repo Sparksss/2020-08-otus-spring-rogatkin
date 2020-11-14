6634bc8a9b11b403a5f08a9e3bd2703f437a7de1
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-11-14 21:17:05 +0300

    add AuthorRepositoryJPA

---
+++ hw06-spring_ORM_JPA/src/main/java/ru/otus/repository/AuthorRepositoryJPA.java
@@ -0,0 +1,19 @@
+package ru.otus.repository;
+
+import ru.otus.models.Author;
+
+import java.util.List;
+
+/*
+ * @created 14/11 - otus-spring
+ * @author Ilya Rogatkin
+ */
+public interface AuthorRepositoryJPA {
+    int countAll();
+    Author add(Author genre);
+    void delete(long id);
+    Author findById(long id);
+    Author findByName(String name);
+    List<Author> getAll();
+    List<Author> getAllAuthorsByBookId(long bookId);
+}
