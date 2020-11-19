33d1e379ea72027c6c7cd1e9a19b4577366a9a2b
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-11-19 15:06:05 +0300

    flyway configuration and disable

--- hw06-spring_ORM_JPA/src/main/java/ru/otus/Application.java
+++ hw06-spring_ORM_JPA/src/main/java/ru/otus/Application.java
@@ -1,11 +1,26 @@
 package ru.otus;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+    private static final String URL = "jdbc:postgresql://localhost:5432/test";
+    private static final String USER = "test";
+    private static final String PASSWORD = "12345";
+
     public static void main(String[] args) {
+        flyWayMigrations(URL, USER , PASSWORD);
         SpringApplication.run(Application.class);
+    }
+
+
+    public static void flyWayMigrations(String url, String user, String password) {
+        Flyway flyway = Flyway.configure()
+                .dataSource(url, user, password)
+                .locations("classpath:/db/migration")
+                .load();
+        flyway.migrate();
     }
 }
