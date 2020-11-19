fd95730afa4be50f0459d9c869f8bfd0c6c831ab
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-11-20 00:46:43 +0300

    fixed parameters in bookService

--- hw06-spring_ORM_JPA/src/main/java/ru/otus/services/BookService.java
+++ hw06-spring_ORM_JPA/src/main/java/ru/otus/services/BookService.java
@@ -1,6 +1,5 @@
 package ru.otus.services;
 
-import ru.otus.models.Author;
 import ru.otus.models.Book;
 
 import java.util.List;
@@ -11,11 +10,11 @@ import java.util.List;
  */
 public interface BookService {
     void addBook(String bookName, String genreName) throws Exception;
-    void addAuthorToBook(Book book, Author author) throws Exception;
+    void addAuthorToBook(long bookId, long authorId) throws Exception;
     void update(long bookId, String bookName) throws Exception;
     List<Book> findAll();
     Book findById(long id) throws Exception;
     int getCountBooks();
-    List<Book> getBooksByAuthor(Author author) throws Exception;
-    List<Book> getBooksByGenre(String genreName) throws Exception;
+    List<Book> getBooksByAuthor(long authorId) throws Exception;
+    List<Book> getBooksByGenre(long genreId) throws Exception;
 }
