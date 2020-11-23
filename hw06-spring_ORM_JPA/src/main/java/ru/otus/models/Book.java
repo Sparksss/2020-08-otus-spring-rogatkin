c3f1e819d54ac289ed7fb7eb8f3794525d48e3b5
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-11-23 14:52:42 +0300

    remove constructor, remove ManyToMany with Authors

--- hw06-spring_ORM_JPA/src/main/java/ru/otus/models/Book.java
+++ hw06-spring_ORM_JPA/src/main/java/ru/otus/models/Book.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.List;
 
 /**
  * Created by ilya on Oct, 2020
@@ -23,14 +22,9 @@ public class Book {
     @Column(name = "name")
     private String name;
 
-    @OneToOne
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "genre_id")
     private Genre genre;
-
-    @ManyToMany(targetEntity = Author.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinTable(name = "books_authors", joinColumns = @JoinColumn(name = "book_id", referencedColumnName = "id"),
-            inverseJoinColumns = @JoinColumn(name = "author_id", referencedColumnName = "id"))
-    private List<Author> authors;
 
     public Book(String name) {
         this.name = name;
@@ -39,30 +33,5 @@ public class Book {
     public Book(long id, String name) {
         this.id = id;
         this.name = name;
-    }
-
-    public Book(long id, String name, Genre genre) {
-        this.id = id;
-        this.name = name;
-        this.genre = genre;
-    }
-
-
-    public String getAuthorsAsString() {
-        StringBuilder authors = new StringBuilder();
-        for(Author author : this.authors) {
-            authors.append(author.getName() + " ,");
-        }
-        return authors.toString();
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", name='" + name + '\n' +
-                ", genre=" + (genre != null ? genre.getName() : "") + '\n' +
-                ", authors=" + (authors != null ? this.getAuthorsAsString() : "")  +
-                '}';
     }
 }
