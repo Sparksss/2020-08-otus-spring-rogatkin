1620f29d2e9b1278ace26df65d239763fceb60db
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-11-23 14:51:47 +0300

    remove Transactional from repository

--- hw06-spring_ORM_JPA/src/main/java/ru/otus/services/AuthorServiceImpl.java
+++ hw06-spring_ORM_JPA/src/main/java/ru/otus/services/AuthorServiceImpl.java
@@ -1,17 +1,18 @@
 package ru.otus.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import ru.otus.models.Author;
 import ru.otus.repository.AuthorRepositoryJPA;
 
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 /*
  * @created 19/11 - otus-spring
  * @author Ilya Rogatkin
  */
-@Component
+@Service
 public class AuthorServiceImpl implements AuthorService {
 
     private AuthorRepositoryJPA authorRepositoryJPA;
@@ -21,6 +22,7 @@ public class AuthorServiceImpl implements AuthorService {
         this.authorRepositoryJPA = authorRepositoryJPA;
     }
 
+    @Transactional
     @Override
     public void save(String authorName) throws Exception {
         if(authorName == null || authorName.isEmpty()) throw new Exception("Author name cannon be empty");
@@ -28,6 +30,7 @@ public class AuthorServiceImpl implements AuthorService {
         this.authorRepositoryJPA.save(author);
     }
 
+    @Transactional
     @Override
     public void update(long id, String authorName) throws Exception {
         if(id == 0) throw new Exception("id cannot be 0");
@@ -40,17 +43,20 @@ public class AuthorServiceImpl implements AuthorService {
         }
     }
 
+    @Transactional
     @Override
     public void delete(long id) throws Exception {
         if(id == 0) throw new Exception("Wrong id");
         this.authorRepositoryJPA.delete(id);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public List<Author> findAll() {
         return this.authorRepositoryJPA.getAll();
     }
 
+    @Transactional(readOnly = true)
     @Override
     public Author findById(long id) throws Exception {
         if(id == 0) throw new Exception("Wrong id");
