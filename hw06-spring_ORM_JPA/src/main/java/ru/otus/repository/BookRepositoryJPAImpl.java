2d1da6bb47d80a9be8bceb5900c10ea8dfe167fc
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-11-23 14:59:53 +0300

    remove Transactional from repo,

--- hw06-spring_ORM_JPA/src/main/java/ru/otus/repository/BookRepositoryJPAImpl.java
+++ hw06-spring_ORM_JPA/src/main/java/ru/otus/repository/BookRepositoryJPAImpl.java
@@ -2,15 +2,12 @@ package ru.otus.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import ru.otus.models.Author;
 import ru.otus.models.Book;
-import ru.otus.models.BookAuthor;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -27,7 +24,6 @@ public class BookRepositoryJPAImpl implements BookRepositoryJPA {
         this.bookAuthorRepositoryJPA = bookAuthorRepositoryJPA;
     }
 
-    @Transactional
     @Override
     public Book save(Book book) {
         if(book.getId() == 0) {
@@ -35,23 +31,14 @@ public class BookRepositoryJPAImpl implements BookRepositoryJPA {
         } else {
           book = em.merge(book);
         }
-        List<Author> authors = book.getAuthors();
-        if(authors != null) {
-            for(Author author : authors) {
-                BookAuthor bookAuthor = new BookAuthor(book.getId(), author.getId());
-                this.bookAuthorRepositoryJPA.save(bookAuthor);
-            }
-        }
         return book;
     }
 
-    @Transactional
     @Override
     public Book findById(long id) {
         return em.find(Book.class, id);
     }
 
-    @Transactional
     @Override
     public Book findByName(String name) {
         TypedQuery<Book> query = em.createQuery("select b from Book b where b.name like :name", Book.class);
@@ -59,13 +46,11 @@ public class BookRepositoryJPAImpl implements BookRepositoryJPA {
         return query.getSingleResult();
     }
 
-    @Transactional
     @Override
     public List<Book> findAll() {
         return em.createQuery("select b from Book b", Book.class).getResultList();
     }
 
-    @Transactional
     @Override
     public void updateNameById(long id, String name) {
         Query query = em.createQuery("update Book b set b.name = :name where b.id = :id");
@@ -74,7 +59,6 @@ public class BookRepositoryJPAImpl implements BookRepositoryJPA {
         query.executeUpdate();
     }
 
-    @Transactional
     @Override
     public void deleteById(long id) {
         Query query = em.createQuery("delete from Book where id = :id");
@@ -82,7 +66,6 @@ public class BookRepositoryJPAImpl implements BookRepositoryJPA {
         query.executeUpdate();
     }
 
-    @Transactional
     @Override
     public List<Book> findAllByAuthor(long authorId) {
         TypedQuery<Book> query = em.createQuery("select b from Book b inner join BookAuthor ba on b.id = ba.bookId inner join Author a on ba.authorId = a.id where a.id = :authorId", Book.class);
@@ -90,7 +73,6 @@ public class BookRepositoryJPAImpl implements BookRepositoryJPA {
         return query.getResultList();
     }
 
-    @Transactional
     @Override
     public List<Book> findAllByGenre(long genreId) {
         TypedQuery<Book> query = em.createQuery("select b from Book b where b.genre.id = :genreId", Book.class);
@@ -98,7 +80,6 @@ public class BookRepositoryJPAImpl implements BookRepositoryJPA {
         return query.getResultList();
     }
 
-    @Transactional
     @Override
     public int countBook() {
         return this.em.createQuery("select count(b) from Book b", int.class).getSingleResult();
