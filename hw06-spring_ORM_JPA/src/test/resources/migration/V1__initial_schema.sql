9849c8bfb2e541f133c2a34beb4fae4fefa4a96a
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-11-25 00:36:33 +0300

    add schema for tests and change type bigserial on int8

--- hw06-spring_ORM_JPA/src/test/resources/migration/V1__initial_schema.sql
+++ hw06-spring_ORM_JPA/src/test/resources/migration/V1__initial_schema.sql
@@ -4,24 +4,23 @@
 -- drop table if exists books_authors cascade;
 
 CREATE TABLE genres (
-    id bigserial primary key,
+    id int8 auto_increment,
     name varchar(200)
 );
 
 CREATE TABLE authors (
-    id bigserial primary key,
+    id int8 auto_increment,
     name varchar(200)
 );
 
 CREATE TABLE books (
-    id bigserial primary key,
+    id int8 auto_increment,
     name varchar(200) not null,
-    genre_id bigserial,
-    FOREIGN KEY (genre_id) references genres(id)
+    genre_id int8 references genres(id)
 );
 
 CREATE TABLE books_authors (
-    id bigserial primary key,
-    book_id bigserial references books(id) ON DELETE CASCADE,
-    author_id bigserial references authors(id) ON DELETE CASCADE
+    id int8 auto_increment,
+    book_id int8 references books(id) ON DELETE CASCADE,
+    author_id int8 references authors(id) ON DELETE CASCADE
 );
