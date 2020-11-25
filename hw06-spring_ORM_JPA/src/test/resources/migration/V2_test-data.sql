eff7b0a9b9e40db90be078d32731e6d635062fdd
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-11-25 23:06:04 +0300

    add test data for books

--- hw06-spring_ORM_JPA/src/test/resources/migration/V2_test-data.sql
+++ hw06-spring_ORM_JPA/src/test/resources/migration/V2_test-data.sql
@@ -1,2 +1,4 @@
-insert into public.genres (name) values ('sci_fiction'), ('Detective');
+insert into public.genres (name) values ('sci_fiction'), ('Detective'), ('Novel');
 insert into public.authors(name) values ('Alexander_Pushkin'), ('Leo_Tolstoy');
+insert into public.books (name, genre_id) values ('The Captains Daughter', 3);
+insert into public.books_authors (book_id, author_id) values (1, 1);
