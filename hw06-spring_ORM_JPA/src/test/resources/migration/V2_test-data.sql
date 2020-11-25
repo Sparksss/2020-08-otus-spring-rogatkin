9e6082725fbb44ad98f4771aecc3a2027769e9b2
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-11-25 13:35:04 +0300

    add two authors in initial for tests

--- hw06-spring_ORM_JPA/src/test/resources/migration/V2_test-data.sql
+++ hw06-spring_ORM_JPA/src/test/resources/migration/V2_test-data.sql
@@ -1,2 +1,2 @@
-insert into public.genres (name) values ('sci_fiction');
-insert into public.genres(name) values ('Detective');
+insert into public.genres (name) values ('sci_fiction'), ('Detective');
+insert into public.authors(name) values ('Alexander_Pushkin'), ('Leo_Tolstoy');
