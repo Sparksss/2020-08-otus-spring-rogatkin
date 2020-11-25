458b8f785147006a0dd5b933636e13e6b692bb70
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-11-26 00:02:05 +0300

    add comments in BookDto

--- hw06-spring_ORM_JPA/src/main/java/ru/otus/dto/BookDto.java
+++ hw06-spring_ORM_JPA/src/main/java/ru/otus/dto/BookDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.otus.models.Author;
+import ru.otus.models.Comment;
 import ru.otus.models.Genre;
 
 import java.util.ArrayList;
@@ -21,4 +22,5 @@ public class BookDto {
     private String name;
     private Genre genre;
     private List<Author> authors = new ArrayList<>();
+    private List<Comment> comments = new ArrayList<>();
 }
