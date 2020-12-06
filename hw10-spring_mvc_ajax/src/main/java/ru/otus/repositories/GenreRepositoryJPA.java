e2b03561c7f4e075296221453cf8d7940b0b3b9c
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-12-06 21:00:12 +0300

    copy files from last project

---
+++ hw10-spring_mvc_ajax/src/main/java/ru/otus/repositories/GenreRepositoryJPA.java
@@ -0,0 +1,19 @@
+package ru.otus.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import ru.otus.entities.Genre;
+
+import java.util.List;
+
+/*
+ * @created 05/12 - otus-spring
+ * @author Ilya Rogatkin
+ */
+public interface GenreRepositoryJPA extends CrudRepository<Genre, Long> {
+    Genre save(Genre genre);
+    void delete(Genre genre);
+    Genre findById(long id);
+    Genre findByName(String name);
+    List<Genre> findAll();
+    long count();
+}
