cd7171f9f2ad7c7a906573dddb1806d1518fffdc
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-12-08 23:27:32 +0300

    add test data for migrations, test, config

---
+++ hw10-spring_mvc_ajax/src/test/java/ru/otus/controllers/BookControllerImplTest.java
@@ -0,0 +1,47 @@
+package ru.otus.controllers;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.otus.entities.Book;
+import ru.otus.entities.Genre;
+import ru.otus.services.BookServiceImpl;
+import ru.otus.services.GenreServiceImpl;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+/*
+ * @created 08/12 - otus-spring
+ * @author Ilya Rogatkin
+ */
+@DisplayName("Контроллер BookControllerImplTest должен")
+@WebMvcTest(BookControllerImpl.class)
+class BookControllerImplTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookServiceImpl bookService;
+
+    @MockBean
+    private GenreServiceImpl genreService;
+
+    @DisplayName("Успешно получать список всех книг")
+    @Test
+    public void getListOfBooks() throws Exception {
+        given(bookService.findById(1))
+                .willReturn(new Book("The Captains Daughter"));
+        given(genreService.findById(1))
+                .willReturn(new Genre("sci_fiction"));
+
+        this.mockMvc.perform(get("/book"))
+                .andExpect(status().isOk());
+    }
+
+
+}
