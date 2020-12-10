a60ecb70627fecba61c77e9a762d27b9cf13804b
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-12-10 14:04:59 +0300

    wrap genre in Optional

--- hw10-spring_mvc_ajax/src/main/java/ru/otus/repositories/GenreRepositoryJPA.java
+++ hw10-spring_mvc_ajax/src/main/java/ru/otus/repositories/GenreRepositoryJPA.java
@@ -3,12 +3,12 @@ package ru.otus.repositories;
 import org.springframework.data.repository.CrudRepository;
 import ru.otus.entities.Genre;
 
-import java.util.List;
+import java.util.Optional;
 
 /*
  * @created 05/12 - otus-spring
  * @author Ilya Rogatkin
  */
 public interface GenreRepositoryJPA extends CrudRepository<Genre, Long> {
-    Genre findByName(String name);
+    Optional<Genre> findByName(String name);
 }
