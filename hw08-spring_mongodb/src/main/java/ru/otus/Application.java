48bd0de66a0c36b9173c1559d4135d4f668575c9
Ilya Rogatkin <toch.in.go@googlemail.com>
2020-12-20 14:01:56 +0300

    add started classes and application config

---
+++ hw08-spring_mongodb/src/main/java/ru/otus/Application.java
@@ -0,0 +1,19 @@
+package ru.otus;
+
+import com.github.cloudyrock.spring.v5.EnableMongock;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+/*
+ * @created 16/12 - otus-spring
+ * @author Ilya Rogatkin
+ */
+@EnableMongock
+@EnableMongoRepositories
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
